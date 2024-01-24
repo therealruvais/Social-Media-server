984c0d9bb338cf87100e2ea0dab9cbe03e1aa430
therealruvais <ruvais666@gmail.com>
2024-01-24 17:37:46 +0530

    new

--- controller/authCtrl.js
+++ controller/authCtrl.js
@@ -41,7 +41,7 @@ const loginUser = async (req, res) => {
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 1),
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", 
-    sameSite: "lax",
+    sameSite: "None",
   });
   res.json({ msg: "success", userToken });
 };
